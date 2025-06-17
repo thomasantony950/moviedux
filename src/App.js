79f7fa98c860d323f74d82524c1d4e2aa33bb45a
Antony Thomas V S <antonythomas.vs@prodapt.com>
2025-06-17 14:37:25 +0530

    MoviesGrid created with json loaded

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import './styles.css';
-import Header from './components/Header';
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
-      </div>  
-      <footer>
-        <p className="footer">Footer content</p>
-      </footer>
+        <MoviesGrid />
+      </div>
+      <Footer />
     </div>
   );
 }
